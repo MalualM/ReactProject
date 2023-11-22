bffedd1f5c18444e14f88435e65983aaaef75481
malual <malual@DESKTOP-8LEI2ON.localdomain>
2023-11-22 11:54:57 +0300

    fetched data from a public api and displayed it to the website

--- onlinestore/src/App.js
+++ onlinestore/src/App.js
@@ -1,17 +1,54 @@
 import './App.css';
 import {BrowserRouter as Router,Routes, Route} from 'react-router-dom' 
 import { Navbar } from './components/navbar';
-function App() {
+import HomePage from './components/HomePage';
+import React,{useState,useEffect} from 'react';
+
+function App  () {
+ const[products,setProducts]=useState([]) 
+  
+ useEffect(()=>{
+  const fetchdata=async()=>{
+    try {
+      const response=await fetch('https://api.escuelajs.co/api/v1/products');
+      const data=await response.json();
+      setProducts(data)
+    } catch (error){
+      console.error('Error fetching data',error)
+    }
+  }
+  fetchdata()
+ },[])
   return (
+    <>
     <div className='App'>
-      <Router>
-        <Navbar/>
-        <Routes>
-          <Route path='/'/>
-          <Route path='/cart'/>
-        </Routes>
-      </Router>
+       
+       <Router>
+         <HomePage/>
+         <Navbar/>
+         <Routes>
+           <Route path='/'/>
+           <Route path='/cart'/>
+         </Routes>
+       </Router>
+     </div>
+    <div>
+      <ul>
+        {products.map((product)=>(
+          <li key={product.id}>
+            <strong>{product.title}
+            </strong> - ${product.price}
+            <strong>
+              {product.image}
+            </strong>
+          </li>
+        ))}
+      </ul>
     </div>
+    
+
+
+    </>
   )
 }
 
